d249553efa7e8ee1b9f74e915da4b8eb30a06730
Khaled-MK <melloukimedkhaled@gmail.com>
2024-06-05 15:32:38 +0100

    Rectification de la recherche avec l'option hidden au lieu de refaire toute la fonction srtDisplay

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const glob = document.getElementById("global");
 const searchBar = document.getElementById("searchBar");
 let sourats;
-let sowar;
 
 window.addEventListener("load", async () => {
    await fetch(`https://api.quran.com/api/v4/chapters`, {
@@ -19,123 +18,25 @@ window.addEventListener("load", async () => {
       .then((data) => {
          srtDisplay(data.chapters);
          let lecBtns = document.querySelectorAll(".btn");
-         lecBtns.forEach((btn) => {
-            btn.addEventListener("click", async () => {
-               let bigDiv = btn.parentElement.parentElement;
-
-               const lecDiv = document.createElement("div");
-               const ctrlDiv = document.createElement("div");
-               const backBtn = document.createElement("button");
-               const playBtn = document.createElement("button");
-               const fwdBtn = document.createElement("button");
-               const stopBtn = document.createElement("button");
-               const backi = document.createElement("i");
-               const playi = document.createElement("i");
-               const fwdi = document.createElement("i");
-               const stopi = document.createElement("i");
-               const audio = document.createElement("audio");
-               const timeSpan = document.createElement("span");
-               const progSpan = document.createElement("span");
-               const curTimeSpan = document.createElement("span");
-               const fullTimeSpan = document.createElement("span");
-
-               lecDiv.classList.add("my-4", "px-4", "flex", "items-center", "justify-center", "gap-x-3", "text-[16px]");
-               ctrlDiv.classList.add("flex", "gap-2", "justify-center");
-               backBtn.classList.add("controls", "bg-green-600", "hover:bg-green-800", "text-white", "px-5", "py-2", "text-xl", "rounded-lg", "flex", "justify-center", "items-center");
-               playBtn.classList.add("controls", "bg-green-100", "hover:bg-green-800", "text-white", "px-5", "py-2", "text-xl", "rounded-lg", "flex", "justify-center", "items-center", "gap-2");
-               fwdBtn.classList.add("controls", "bg-green-600", "hover:bg-green-800", "text-white", "px-5", "py-2", "text-xl", "rounded-lg", "flex", "justify-center", "items-center");
-               stopBtn.classList.add("controls", "bg-green-600", "hover:bg-green-800", "text-white", "px-5", "py-2", "text-xl", "rounded-lg", "flex", "justify-center", "items-center");
-               backi.classList.add("fa", "fa-step-forward");
-               playi.classList.add("fa", "fa-play", "rot");
-               fwdi.classList.add("fa", "fa-step-backward");
-               stopi.classList.add("fa", "fa-stop");
-               timeSpan.classList.add("w-full", "relative", "rounded-full", "h-[6px]", "bg-gray-300", "cursor-pointer");
-               progSpan.classList.add("absolute", "top-0", "right-0", "rounded-full", "h-[6px]", "bg-green-600");
-
-               curTimeSpan.textContent = "0:00";
-               timeSpan.append(progSpan);
-               lecDiv.append(curTimeSpan, timeSpan, fullTimeSpan, audio);
-
-               backBtn.append(backi);
-               playBtn.append(playi);
-               fwdBtn.append(fwdi);
-               stopBtn.append(stopi);
-               ctrlDiv.append(backBtn, playBtn, fwdBtn, stopBtn);
-               bigDiv.append(lecDiv, ctrlDiv);
-
-               bigDiv.classList.add("col-span-2", "gap-y-10");
-               bigDiv.children[3].remove();
-               bigDiv.children[2].remove();
-               bigDiv.children[1].remove();
-
-               audio.src = `https://download.quranicaudio.com/qdc/hani_ar_rifai/murattal/${bigDiv.id}.mp3`;
-               audio.classList.add("audio");
-               // audio.setAttribute("controls", "");
-
-               audio.addEventListener("loadedmetadata", () => {
-                  fullTimeSpan.append(document.createTextNode(formatTime(audio.duration)));
-               });
-               audio.addEventListener("canplay", () => {
-                  playBtn.classList.add("bg-green-600");
-               });
-               backBtn.addEventListener("click", () => {
-                  audio.currentTime = parseFloat(audio.currentTime) - 10;
-                  // console.log(audio.duration);
-               });
-               fwdBtn.addEventListener("click", () => {
-                  audio.currentTime = parseFloat(audio.currentTime) + 10;
-               });
-
-               playBtn.addEventListener("click", () => {
-                  let audios = document.querySelectorAll(".audio");
-                  console.log(audios);
-                  let playsbtn = document.querySelectorAll(".fa-pause");
-
-                  for (let i = 0; i < audios.length; i++) {
-                     audios[i].pause();
-                     console.log(audios[i]);
-                  }
-
-                  if (playi.classList.contains("fa-play")) {
-                     playi.classList.add("fa-pause");
-                     playi.classList.remove("fa-play");
-                     audio.play();
-                  } else {
-                     playi.classList.remove("fa-pause");
-                     playi.classList.add("fa-play");
-                     audio.pause();
-                  }
-                  for (let i = 0; i < playsbtn.length; i++) {
-                     playsbtn[i].classList.add("fa-play");
-                     playsbtn[i].classList.remove("fa-pause");
-                  }
-               });
-
-               stopBtn.addEventListener("click", () => {
-                  playi.classList.remove("fa-pause");
-                  playi.classList.add("fa-play");
-                  audio.pause();
-                  audio.currentTime = 0;
-               });
-               audio.addEventListener("timeupdate", async () => {
-                  progSpan.style.width = `${(audio.currentTime * 100) / audio.duration}%`;
-                  curTimeSpan.textContent = formatTime(audio.currentTime);
-                  if (audio.currentTime === audio.duration) {
-                     playi.classList.remove("fa-pause");
-                     playi.classList.add("fa-play");
-                  }
-               });
-               timeSpan.addEventListener("click", (e) => {
-                  const spanWidth = e.target.getBoundingClientRect().width;
-                  pxPosition = e.clientX - e.target.getBoundingClientRect().left;
-                  position = (spanWidth - pxPosition) / spanWidth;
-                  progSpan.style.width = `${position * 100}%`;
-                  audio.currentTime = position * audio.duration;
-                  // console.log(position * audio.duration);
-               });
-            });
-         });
+         lecture(lecBtns);
       });
+});
+
+searchBar.addEventListener("input", () => {
+   let sowar = document.querySelectorAll(".soura");
+   const input = searchBar.value;
+
+   sowar.forEach((soura) => {
+      let arTitre = soura.firstChild.firstChild.textContent;
+      let frTitre = soura.firstChild.lastChild.textContent.toLocaleLowerCase();
+
+      if (arTitre.includes(input) || frTitre.includes(input.toLocaleLowerCase())) {
+         soura.classList.remove("hidden");
+         console.log(soura);
+      } else {
+         soura.classList.add("hidden");
+      }
+   });
 });
 
 function formatTime(seconds) {
@@ -146,7 +47,7 @@ function formatTime(seconds) {
 
 function srtDisplay(data) {
    sourats = data;
-   sowar = data;
+
    data.forEach((soura) => {
       const bigDiv = document.createElement("div");
       const titreDiv = document.createElement("div");
@@ -167,7 +68,7 @@ function srtDisplay(data) {
       const arVerses = document.createTextNode("الآيات");
       const verses = document.createTextNode(soura.verses_count);
 
-      bigDiv.classList.add("bg-white", "rounded-xl", "shadow-md", "px-3", "pt-6", "pb-2", "gap-y-10", "text-xl");
+      bigDiv.classList.add("bg-white", "rounded-xl", "shadow-md", "px-3", "pt-6", "pb-2", "gap-y-10", "text-xl", "soura");
       titreDiv.classList.add("my-3", "text-center", "font-semibold");
       arH.classList.add("arTitre", "text-2xl");
       frH.classList.add("frTitre", "text-xl");
@@ -206,29 +107,132 @@ function srtDisplay(data) {
 
       glob.append(bigDiv);
    });
+   return sourats;
 }
 
-searchBar.addEventListener("keyup", (e) => {
-   console.log(sourats);
-   const input = searchBar.value;
-   let results = [];
+function lecture(lecBtns) {
+   lecBtns.forEach((btn) => {
+      btn.addEventListener("click", async () => {
+         let bigDiv = btn.parentElement.parentElement;
 
-   if (input.length) {
-      results = sowar.filter((key) => {
-         return key.name_simple.toLowerCase().includes(input.toLowerCase()) || key.name_arabic.includes(input);
+         const lecDiv = document.createElement("div");
+         const ctrlDiv = document.createElement("div");
+         const backBtn = document.createElement("button");
+         const playBtn = document.createElement("button");
+         const fwdBtn = document.createElement("button");
+         const stopBtn = document.createElement("button");
+         const backi = document.createElement("i");
+         const playi = document.createElement("i");
+         const fwdi = document.createElement("i");
+         const stopi = document.createElement("i");
+         const audio = document.createElement("audio");
+         const timeSpan = document.createElement("span");
+         const progSpan = document.createElement("span");
+         const curTimeSpan = document.createElement("span");
+         const fullTimeSpan = document.createElement("span");
+
+         lecDiv.classList.add("my-4", "px-4", "flex", "items-center", "justify-center", "gap-x-3", "text-[16px]");
+         ctrlDiv.classList.add("flex", "gap-2", "justify-center");
+         backBtn.classList.add("controls", "bg-green-600", "hover:bg-green-800", "text-white", "px-5", "py-2", "text-xl", "rounded-lg", "flex", "justify-center", "items-center");
+         playBtn.classList.add("controls", "bg-green-100", "hover:bg-green-800", "text-white", "px-5", "py-2", "text-xl", "rounded-lg", "flex", "justify-center", "items-center", "gap-2");
+         fwdBtn.classList.add("controls", "bg-green-600", "hover:bg-green-800", "text-white", "px-5", "py-2", "text-xl", "rounded-lg", "flex", "justify-center", "items-center");
+         stopBtn.classList.add("controls", "bg-green-600", "hover:bg-green-800", "text-white", "px-5", "py-2", "text-xl", "rounded-lg", "flex", "justify-center", "items-center");
+         backi.classList.add("fa", "fa-step-forward");
+         playi.classList.add("fa", "fa-play", "rot");
+         fwdi.classList.add("fa", "fa-step-backward");
+         stopi.classList.add("fa", "fa-stop");
+         timeSpan.classList.add("w-full", "relative", "rounded-full", "h-[6px]", "bg-gray-300", "cursor-pointer");
+         progSpan.classList.add("absolute", "top-0", "right-0", "rounded-full", "h-[6px]", "bg-green-600");
+
+         curTimeSpan.textContent = "0:00";
+         timeSpan.append(progSpan);
+         lecDiv.append(curTimeSpan, timeSpan, fullTimeSpan, audio);
+
+         backBtn.append(backi);
+         playBtn.append(playi);
+         fwdBtn.append(fwdi);
+         stopBtn.append(stopi);
+         ctrlDiv.append(backBtn, playBtn, fwdBtn, stopBtn);
+         bigDiv.append(lecDiv, ctrlDiv);
+
+         bigDiv.classList.add("col-span-2", "gap-y-10");
+         bigDiv.children[3].remove();
+         bigDiv.children[2].remove();
+         bigDiv.children[1].remove();
+
+         audio.src = `https://download.quranicaudio.com/qdc/hani_ar_rifai/murattal/${bigDiv.id}.mp3`;
+         audio.classList.add("audio");
+         // audio.setAttribute("controls", "");
+
+         audio.addEventListener("loadedmetadata", () => {
+            fullTimeSpan.append(document.createTextNode(formatTime(audio.duration)));
+         });
+         audio.addEventListener("canplay", () => {
+            playBtn.classList.add("bg-green-600");
+         });
+         backBtn.addEventListener("click", () => {
+            audio.currentTime = parseFloat(audio.currentTime) - 10;
+            // console.log(audio.duration);
+         });
+         fwdBtn.addEventListener("click", () => {
+            audio.currentTime = parseFloat(audio.currentTime) + 10;
+         });
+
+         playBtn.addEventListener("click", () => {
+            let audios = document.querySelectorAll(".audio");
+            console.log(audios);
+            let playsbtn = document.querySelectorAll(".fa-pause");
+
+            for (let i = 0; i < audios.length; i++) {
+               audios[i].pause();
+               console.log(audios[i]);
+            }
+
+            if (playi.classList.contains("fa-play")) {
+               playi.classList.add("fa-pause");
+               playi.classList.remove("fa-play");
+               audio.play();
+            } else {
+               playi.classList.remove("fa-pause");
+               playi.classList.add("fa-play");
+               audio.pause();
+            }
+            for (let i = 0; i < playsbtn.length; i++) {
+               playsbtn[i].classList.add("fa-play");
+               playsbtn[i].classList.remove("fa-pause");
+            }
+         });
+
+         stopBtn.addEventListener("click", () => {
+            playi.classList.remove("fa-pause");
+            playi.classList.add("fa-play");
+            audio.pause();
+            audio.currentTime = 0;
+         });
+         audio.addEventListener("timeupdate", async () => {
+            progSpan.style.width = `${(audio.currentTime * 100) / audio.duration}%`;
+            curTimeSpan.textContent = formatTime(audio.currentTime);
+            if (audio.currentTime === audio.duration) {
+               playi.classList.remove("fa-pause");
+               playi.classList.add("fa-play");
+            }
+         });
+         timeSpan.addEventListener("click", (e) => {
+            const spanWidth = e.target.getBoundingClientRect().width;
+            pxPosition = e.clientX - e.target.getBoundingClientRect().left;
+            position = (spanWidth - pxPosition) / spanWidth;
+            progSpan.style.width = `${position * 100}%`;
+            audio.currentTime = position * audio.duration;
+            // console.log(position * audio.duration);
+         });
       });
+   });
+}
 
-      for (let i = glob.children.length - 1; i >= 0; i--) {
-         glob.children[i].remove();
-      }
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-      if (results.length) {
-         srtDisplay(results);
-      } else {
-         srtDisplay(sourats);
-      }
-   } else {
-      srtDisplay(sourats);
-   }
-});
-//rest a trouver le moyen de garder sourats a 214 element
+// let b = a.filter((num) => {
+//    return num % 2 === 0;
+// });
+// console.log(b);
+// console.log(a);
